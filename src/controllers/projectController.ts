f266b129eb26a7dcf6b09217261d6219b88a3bfc
tchesa <cesarolimpio@gmail.com>
2023-12-10 12:28:35 -0300

    typeorm: create & list projects

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -3,14 +3,70 @@ import auth from "../middlewares/auth";
 import Project from "../entities/Project";
 import Task from "../entities/Task";
 import { AppDataSource } from "../database";
+import User from "../entities/User";
+import { ObjectId } from "mongodb";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get("/", async (_, res) => {
   try {
     const repo = AppDataSource.getMongoRepository(Project);
-    // const projects = await Project.find().populate(["user", "tasks"]);
+    const userRepo = AppDataSource.getMongoRepository(User);
+    const taskRepo = AppDataSource.getMongoRepository(Task);
+
     const projects = await repo.find();
+
+    const userIds: string[] = [];
+    const taskIds: string[] = [];
+
+    projects.forEach((project) => {
+      userIds.push(project.user.toString());
+      taskIds.push(...project.tasks.map((task) => task.toString()));
+    });
+
+    const usersSet = new Set(userIds);
+    const tasksSet = new Set(taskIds);
+
+    const users = await Promise.all(
+      Array.from(usersSet).map((userId) =>
+        userRepo.findOneBy({
+          _id: new ObjectId(userId),
+        })
+      )
+    );
+
+    const tasks = await Promise.all(
+      Array.from(tasksSet).map((taskId) =>
+        taskRepo.findOneBy({
+          _id: new ObjectId(taskId),
+        })
+      )
+    );
+
+    const usersMap = users.reduce<Record<string, User>>((map, user) => {
+      return {
+        ...map,
+        [user._id.toString()]: user,
+      };
+    }, {});
+
+    const tasksMap = tasks.reduce<Record<string, Task>>((map, task) => {
+      return {
+        ...map,
+        [task._id.toString()]: task,
+      };
+    }, {});
+
+    projects.forEach((project) => {
+      project.user = usersMap[project.user.toString()];
+
+      for (let i = 0; i < project.tasks.length; i++) {
+        project.tasks[i] = tasksMap[project.tasks[i].toString()];
+      }
+
+      project.user.password = undefined;
+    });
+
     return res.send(projects);
   } catch (error) {
     console.log(error);
@@ -18,41 +74,73 @@ router.get("/", async (req, res) => {
   }
 });
 
-// router.get("/:id", async (req, res) => {
-//   const { id } = req.params;
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
 
-//   try {
-//     const project = await Project.findById(id).populate(["user", "tasks"]);
-//     return res.send(project);
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).send({ error: "Server error" });
-//   }
-// });
+  try {
+    const repo = AppDataSource.getMongoRepository(Project);
+    const taskRepo = AppDataSource.getMongoRepository(Task);
+    const userRepo = AppDataSource.getMongoRepository(User);
+    const project = await repo.findOneBy({ _id: new ObjectId(id) });
 
-// router.post("/", async (req, res) => {
-//   const { userId, title, description, tasks } = req.body;
+    const tasks = await Promise.all(
+      project.tasks.map((taskId) =>
+        taskRepo.findOneBy({ _id: new ObjectId(taskId.toString()) })
+      )
+    );
 
-//   try {
-//     const project = await Project.create({ title, description, user: userId });
+    const user = await userRepo.findOneBy({
+      _id: new ObjectId(project.user.toString()),
+    });
 
-//     const createdTasks = await Promise.all(
-//       tasks.map((task: any) => {
-//         const projectTask = new Task({ ...task, project: project._id });
+    user.password = undefined;
+    project.user = user;
 
-//         return projectTask.save();
-//       })
-//     );
+    project.tasks.forEach((_, i) => {
+      project.tasks[i] = tasks[i];
+    });
 
-//     createdTasks.forEach((task) => project.tasks.push(task));
-//     await project.save();
+    return res.send(project);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: "Server error" });
+  }
+});
 
-//     return res.send(project);
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).send({ error: "Server error" });
-//   }
-// });
+router.post("/", async (req, res) => {
+  const { userId, title, description, tasks } = req.body;
+
+  try {
+    const repo = AppDataSource.getMongoRepository(Project);
+    const taskRepo = AppDataSource.getMongoRepository(Task);
+
+    const project = repo.create({
+      title,
+      description,
+      user: new ObjectId(userId),
+    });
+
+    await repo.save(project);
+
+    const createdTasks = tasks.map((task) =>
+      taskRepo.create({
+        ...task,
+        project: project._id,
+        completed: task.completed ?? false,
+      })
+    );
+
+    await Promise.all(createdTasks.map((task) => taskRepo.save(task)));
+
+    project.tasks = createdTasks.map((task) => task._id);
+    await repo.save(project);
+
+    return res.send(project);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: "Server error" });
+  }
+});
 
 // router.put("/:id", async (req, res) => {
 //   const { userId, title, description, tasks } = req.body;
